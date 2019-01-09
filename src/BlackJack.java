7c10944ab5122cdd2f35c4ba209088ac146106ec
ian <ian.douglas.mckellar@gmail.com>
2019-01-09 12:17:38 -0700

    Figuring things out

---
+++ src/BlackJack.java
@@ -0,0 +1,6 @@
+public class BlackJack {
+    public static void main(String[] args) {
+        System.out.println("Just putting something here");
+        System.out.println("Hello, world!");
+    }
+}
